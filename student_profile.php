1fd50f3ed81ad2b8b243bd7fec8980ee600a2545
Nishi Doshi <doshinishi1612@gmail.com>
2018-02-12 00:53:51 +0530

    Add files via upload
    Student Profile created without frontend Only backend involved.

---
+++ student_profile.php
@@ -0,0 +1,52 @@
+
+<?php
+	session_start();
+	if(!$_SESSION['user']){
+		header('location:signup_student.php');
+	}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+	</head>
+	<body>
+		<?php
+		include ('connsp.php');	 
+			$user= $_SESSION['user'];
+			$query = "select * from student s where user='$user'";
+			$res=$connsp->query($query);
+			if($fe=$res->fetch_object()){
+				$user = $fe->user;
+				$pass = $fe->pass;
+				$studentid = $fe->studentid;
+ 				$email = $fe->email; 	
+			 	$phone_no = $fe->phone_no;
+			 	$bloodg=$fe->bloodg;
+    			$address = $fe->address;
+			} 
+		?>
+		<table>
+			<tr>
+				<td>Id :</td>
+				<td><?php echo $studentid ?></td>
+			</tr>
+			<tr>
+				<td>Username :</td>
+				<td><?php echo $user ?></td>
+			</tr>
+			<tr>
+				<td>Email :</td>
+				<td><?php echo $email ?></td>
+			</tr>
+			<tr>
+				<td>Phone Number :</td>
+				<td><?php echo $phone_no ?></td>
+			</tr>
+
+			<tr>
+				<td>Blood Group :</td>
+				<td><?php echo $bloodg ?></td>
+			</tr>
+		</table>
+	</body>
+</html>
