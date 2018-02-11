3f0da5a0ad6fbfee8dc0ad24153b837670f0111f
Nishi Doshi <doshinishi1612@gmail.com>
2018-02-12 01:22:46 +0530

    Add files via upload
    SQL files updated with primary key

--- student.sql
+++ student.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 11, 2018 at 04:12 PM
--- Server version: 10.1.28-MariaDB
--- PHP Version: 7.1.11
+-- Host: localhost
+-- Generation Time: Feb 11, 2018 at 08:50 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,7 +28,9 @@ SET time_zone = "+00:00";
 -- Table structure for table `student`
 --
 
+DROP TABLE IF EXISTS `student`;
 CREATE TABLE `student` (
+  `pid` int(11) NOT NULL,
   `user` text NOT NULL,
   `pass` text NOT NULL,
   `studentid` text NOT NULL,
@@ -38,7 +40,33 @@ CREATE TABLE `student` (
   `bloodg` text NOT NULL,
   `address` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-COMMIT;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`pid`, `user`, `pass`, `studentid`, `email`, `phone_no`, `confirm_pass`, `bloodg`, `address`) VALUES
+(1, 'nishi1612', 'hello', '201601408', '201601408@daiict.ac.in', 1234567890, '', 'O +ve', 'K106');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- teacher.sql
+++ teacher.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 11, 2018 at 03:39 PM
--- Server version: 10.1.28-MariaDB
--- PHP Version: 7.1.11
+-- Host: localhost
+-- Generation Time: Feb 11, 2018 at 08:51 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,7 +28,9 @@ SET time_zone = "+00:00";
 -- Table structure for table `teacher`
 --
 
+DROP TABLE IF EXISTS `teacher`;
 CREATE TABLE `teacher` (
+  `pid` int(11) NOT NULL,
   `user` text NOT NULL,
   `pass` text NOT NULL,
   `empid` int(100) NOT NULL,
@@ -37,7 +39,33 @@ CREATE TABLE `teacher` (
   `bloodg` text NOT NULL,
   `address` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-COMMIT;
+
+--
+-- Dumping data for table `teacher`
+--
+
+INSERT INTO `teacher` (`pid`, `user`, `pass`, `empid`, `email`, `phone_no`, `bloodg`, `address`) VALUES
+(1, 'nishi1612', '1234', 23416, 'dean@daiict.ac.in', 1234567890, 'B -ve', 'H 108\r\nGandhinagar');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `teacher`
+--
+ALTER TABLE `teacher`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `teacher`
+--
+ALTER TABLE `teacher`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
